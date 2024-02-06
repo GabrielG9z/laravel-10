e89ecac3ec719715156f64f9db31cdc400bfafd4
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2024-02-06 11:57:13 -0300

    Update, criados a listagem dinâmica
    Exibição dos items, via id, e manipulação dos dados sendo realizada

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -16,9 +16,27 @@ class SupportController extends Controller
         return view('admin/supports/index',compact('supports'));
     }
 
+    public function show(string|int $id)
+    {
+        //Support::find($id)
+        //Support::where('id', $id)->first();
+        //Support::where('id', '!=', $id)->first();
+        if(!$support = Support::find($id)) {
+            return back();
+        }
+        return view('admin/supports/show', compact('support'));
+    }
+
     public function create()
     {
         return view('admin/supports/create');
+    }
+
+    public function edit(Support $support, string|int $id)
+    {
+        if(!$support = $support->where('id',$id)->first()) {
+            return back();
+        }
     }
 
     public function store(Request $request, Support $support)
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Http\Controllers\Admin\SupportController;
 use App\Models\Support;
 use Illuminate\Support\Facades\Route; 
 
-Route::post('supports', [SupportController::class, 'store'])->name('supports.store');
+
+//Rota create deve ser colocada antes, da rota get que captura id's dinâmicos para evitar erros!
 Route::get('supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+Route::post('supports', [SupportController::class, 'store'])->name('supports.store');
 Route::get('supports', [SupportController::class, 'index'])->name('supports.index');
 Route::get('/contato', [SiteController::class, 'contact']);
 Route::get('/', function () {
