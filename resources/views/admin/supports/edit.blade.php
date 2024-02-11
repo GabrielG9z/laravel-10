b03bf2ead13de68b1211b58d067fcf74f2c7df92
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2024-02-11 12:19:38 -0300

    Refatorando alguns components

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,16 +1,13 @@
 <h1>Dúvida {{ $support->id }}</h1>
 
-@if ($errors->any())
-    @foreach($errors->all() as $error)
-    {{$error}}
-    @endforeach
-    @endif 
+<x-alert/>
+
 
 <form action="{{route('supports.update', $support->id)}}" method="POST">
     {{-- <input type="hidden" value="{{ csrf_token()}}" name=" _token"> CTRL+K+C= COMENTÁRIO LARAVEL--}}  
     @csrf
     @method('put')
-    <input type="text" placeholder="Assunto" name="subject" value="{{$support->subject}}">
-    <textarea name="body" id="" cols="30" rows="5" placeholder="Descrição" >{{ $support->body}}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', [
+        'support'=> $support
+    ])
 </form>
